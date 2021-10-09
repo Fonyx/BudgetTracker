af57e860ef0098af1032b5e43db40adb6afb928e
Nick <nick.alex.ritchie@gmail.com>
2021-10-09 15:36:40 +1100

    some config updates to mongo env variables

--- server.js
+++ server.js
@@ -2,27 +2,41 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const dotenv = require('dotenv');
+const Logger = require("./libs/logger");
+const configuredMorgan = require("./config/morgan");
+dotenv.config();
 
-const PORT = 3000;
+const db_url = process.env.MONGODB_URL;
+const PORT = process.env.SERVER_PORT;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
+app.use(configuredMorgan);
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+// DATABASE CONNECTION
+const connectionParams={
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true 
+}
+
+mongoose.connect(db_url, connectionParams)
+.then(() => {
+    Logger.info(`Connected to database @ BudgetTracker.uvvp5.mongodb.net`);
+    app.listen(PORT, () => {
+        Logger.info('Server is running http://localhost:'+PORT);
+    });
+})
+.catch( (err) => {
+    Logger.error(`Error connecting to the database. \n${err}`);
+})
