19c0536b11174406ba4f4b0204a56f2629f54f21
Nick <nick.alex.ritchie@gmail.com>
2021-10-12 11:20:03 +1100

    reverting to morgan winston logger

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const dotenv = require('dotenv');
-// const Logger = require("./libs/logger");
-// const configuredMorgan = require("./config/morgan");
+const Logger = require("./libs/logger");
+const configuredMorgan = require("./config/morgan");
 const apiRoute = require('./routes/api')
 dotenv.config();
 
 const db_url = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 28017;
-console.log(`HTTP Serving on port: ${PORT}`);
+Logger.info(`HTTP Serving on port: ${PORT}`);
 
 const app = express();
 
-// app.use(configuredMorgan);
+app.use(configuredMorgan);
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -32,12 +32,11 @@ const connectionParams={
 
 mongoose.connect(db_url, connectionParams)
 .then(() => {
-    console.log(`Connected to database @ BudgetTracker.uvvp5.mongodb.net`);
+    Logger.info(`Connected to database @ BudgetTracker.uvvp5.mongodb.net`);
     app.listen(PORT, () => {
-        console.log('stop logging');
-        // console.log('Server is running with port:'+PORT);
+        Logger.info('Server is running http://localhost:'+PORT);
     });
 })
 .catch( (err) => {
-    console.log(`Error connecting to the database. \n${err}`);
+    Logger.error(`Error connecting to the database. \n${err}`);
 })
